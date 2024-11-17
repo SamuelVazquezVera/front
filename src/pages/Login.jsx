5aeb6b1d8001fd10948c61b67d9d2e08bdab3817
svazquez <svazquez@ipcom.com.mx>
2024-11-17 12:11:56 -0500

    add log

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,6 +14,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
+      console.log( `${uri}auth/login`)
       const response = await axios.post(
         `${uri}auth/login`,
         {
